f836946c2050c8f76f0020ce50f6d770c915b450
Albina Gorelova <albinamedis@yandex.ru>
2023-08-27 17:58:41 +0300

    Выполнение задания 6го спринта

---
+++ src/main/java/qa_scooter/praktikum/constants/Url.java
@@ -0,0 +1,5 @@
+package qa_scooter.praktikum.constants;
+
+public class Url {
+    public static final String BASE_URI = "https://qa-scooter.praktikum-services.ru/";
+}
---
+++ src/main/java/qa_scooter/praktikum/order/Order.java
@@ -0,0 +1,19 @@
+package qa_scooter.praktikum.order;
+
+import java.util.List;
+
+public class Order {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String metroStation;
+    private String phone;
+    private int number;
+    private String deliveryDate;
+    private String comment;
+    private String[] color;
+
+    public Order(String name, String surname, String address, String metroStation, String phone, int number, String deliveryDate, String comments, List<String> color) {
+    }
+
+}
